20a986f4403f6be87cdeb15b047d5291ff0a8296
Jason Basuil <basuilj@gmail.com>
2019-01-02 19:24:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,8 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |nested_key, nested_value| 
       nested_value.each do |pigeon_name|
-        pigeon_list << pigeon_name
+        if pigeon_list[pigeon_name] == nil
+          pigeon_list << pigeon_name
       end 
     end 
     
