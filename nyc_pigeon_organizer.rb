b7447d693dabde71fb438052d61d534f944edc8d
Jason Basuil <basuilj@gmail.com>
2019-01-02 19:09:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,8 +4,9 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.collect do |symbol, attribute|
     symbol.collect do |attribute, array|
-      pigeon_list << array 
       binding.pry
+      pigeon_list << array 
+      
     end 
   end 
 end
